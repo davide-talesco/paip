309f61f970aad64d7565afa1c07e4078e4734c2a
Davide Talesco (IS-IE) <davide_talesco@trendmicro.ie>
2018-08-24 14:41:13 +0100

    updated response Factory to correclty handle error prop

--- index.js
+++ index.js
@@ -235,7 +235,7 @@ const Response = stampit(Message, {
   initializers: [
     function({ error, payload, to }){
       if (error) {
-        this.error = JSON.parse(Errio.stringify(error));
+        this.error = typeof error === 'string' ? error : Errio.stringify(error);
         // if error has statusCode use it otherwise set it to 500
         error.statusCode
           ? (this.statusCode = error.statusCode)
@@ -263,7 +263,7 @@ const Response = stampit(Message, {
     getStatusCode : function(){ return this.statusCode },
     getPayload : function(){
       if (this.statusCode === 200) return _.cloneDeep(this.payload);
-      throw Errio.fromObject(this.error);
+      throw Errio.parse(this.error);
     }
   }
 });
